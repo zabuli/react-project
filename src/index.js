27199ec80b27a329b3af9a823c96ca26120a93b5
zabuli <maria.nemsilajova@gmail.com>
2023-09-13 12:03:35 +0200

    Initial commit.

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  element
 );
 
 // If you want to start measuring performance in your app, pass a function
